1b3790389280abdf6b25fb44cd04197715ab9609
ragnakuei <ragnakuei@livemail.tw>
2016-06-05 18:59:59 +0800

    0605-1900

--- ERP/Models/Book.cs
+++ ERP/Models/Book.cs
@@ -1,10 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ERP.Models
 {
     public class Book
     {
-        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key,Column(Order = 1)]
+        public Guid rowid { get; set; }
+
+        [Key,Column(Order = 2)]
         public int id { get; set; }
 
         [Required]
