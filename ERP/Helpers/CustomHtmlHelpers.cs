10ffe876cbc4ab6024fe6ba207f4051dcf9b6373
Kuei <Kuei@KUEI-PC>
2016-06-20 16:30:15 +0800

    0620-1630

---
+++ ERP/Helpers/CustomHtmlHelpers.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+using System.Web.Mvc;
+
+namespace ERP.Helpers
+{
+    public static class CustomHtmlHelpers
+    {
+        public static MvcHtmlString HightlightKeyWord(this HtmlHelper helper, string SourceStr,string Keyword,string ClassName)
+        {
+            string RegexPattern = "(("+Keyword+")+)";
+            string RegexReplace = "<span class=\"" + ClassName + "\">$1</span>";
+
+            if(Regex.Match(SourceStr, RegexPattern).Success)
+            {
+                return MvcHtmlString.Create(Regex.Replace(SourceStr, RegexPattern, RegexReplace));
+            }
+            else
+            {
+                return MvcHtmlString.Create(SourceStr);
+            }
+        }
+    }
+}
